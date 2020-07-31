bf8975a2a2473873a0225a3005b354b9afc28e94
Phien <phien.hoangnguyen@gmail.com>
2020-07-31 15:30:37 +0700

    upload image in profile page

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,27 +1,39 @@
 import React, { Component, Fragment } from "react";
 import PropTypes from "prop-types";
 import dayjs from "dayjs";
+import { Link } from "react-router-dom";
 //MUI
 import Button from "@material-ui/core/Button";
-import MuiLink from '@material-ui/core/Link';
+import MuiLink from "@material-ui/core/Link";
 import Typography from "@material-ui/core/Typography";
 import withStyles from "@material-ui/core/styles/withStyles";
 import Paper from "@material-ui/core/Paper";
+import IconButton from "@material-ui/core/IconButton";
+import Tooltip from "@material-ui/core/Tooltip";
 //Mui icon
-import LocationOn from '@material-ui/icons/LocationOn';
-import LinkIcon from '@material-ui/icons/Link';
-import CalendarToday from '@material-ui/icons/CalendarToday';
+import LocationOn from "@material-ui/icons/LocationOn";
+import LinkIcon from "@material-ui/icons/Link";
+import CalendarToday from "@material-ui/icons/CalendarToday";
+import EditIcon from "@material-ui/icons/Edit";
 //redux
 import { connect } from "react-redux";
-import { Link } from 'react-router-dom';
-
-
+import { logoutUser, uploadImage } from "../../redux/actions/userActions";
 
 const styles = (theme) => ({
-    ...theme.spread,
-  });
+  ...theme.spread,
+});
 
 class Profile extends Component {
+  handleImageChange = (event) => {
+    const image = event.target.files[0];
+    const formData = new FormData();
+    formData.append("image", image, image.name);
+    this.props.uploadImage(formData);
+  };
+  handleEditPicture = () => {
+    const fileInput = document.getElementById("imageInput");
+    fileInput.click();
+  };
   render() {
     const {
       classes,
@@ -30,57 +42,88 @@ class Profile extends Component {
         loading,
         authenticated,
       },
-    } = this.props; 
+    } = this.props;
 
-    let profileMarkUp = !loading ? (authenticated ? (
+    let profileMarkUp = !loading ? (
+      authenticated ? (
         <Paper className={classes.paper}>
-            <div className={classes.profile}>
-                <div className="image-wrapper">
-                    <img src={imageUrl} alt="propfile" className="profile-image"/>
-                </div>
-                <hr/>
-                <div className="profile-details">
-        <MuiLink component={Link} to={`/users/${handle}`} color="primary" variant="h5">
-            @{handle}
-        </MuiLink>
-        <hr/>
-        {bio && <Typography variant="body2">{bio}</Typography>}
-        <hr/>
-        {location && (
-            <Fragment>
-                <LocationOn color="primary" /> <span>{location}</span>
-                <hr/>
-            </Fragment>
-        )}
-        {website && (
-            <Fragment>
-                <LinkIcon color="primary" />
-                <a href={website} target="_blank" rel="noopener noreferrer">
-                    {' '}{website}
-                </a>
-                <hr/>
-            </Fragment>
-        )}
-        <CalendarToday color="primary" />{' '}
-        <span>Joined {dayjs(createdAt).format('MMM YYYY')}</span>
-                </div>
+          <div className={classes.profile}>
+            <div className="image-wrapper">
+              <img src={imageUrl} alt="propfile" className="profile-image" />
+              <input
+                type="file"
+                id="imageInput"
+                onChange={this.handleImageChange}
+                hidden="hidden"
+              />
+              <Tooltip title="Edit profile picture" placement="right">
+                <IconButton onClick={this.handleEditPicture} className="button">
+                  <EditIcon color="primary" />
+                </IconButton>
+              </Tooltip>
             </div>
+            <hr />
+            <div className="profile-details">
+              <MuiLink
+                component={Link}
+                to={`/users/${handle}`}
+                color="primary"
+                variant="h5"
+              >
+                @{handle}
+              </MuiLink>
+              <hr />
+              {bio && <Typography variant="body2">{bio}</Typography>}
+              <hr />
+              {location && (
+                <Fragment>
+                  <LocationOn color="primary" /> <span>{location}</span>
+                  <hr />
+                </Fragment>
+              )}
+              {website && (
+                <Fragment>
+                  <LinkIcon color="primary" />
+                  <a href={website} target="_blank" rel="noopener noreferrer">
+                    {" "}
+                    {website}
+                  </a>
+                  <hr />
+                </Fragment>
+              )}
+              <CalendarToday color="primary" />{" "}
+              <span>Joined {dayjs(createdAt).format("MMM YYYY")}</span>
+            </div>
+          </div>
         </Paper>
+      ) : (
+        <Paper className={classes.paper}>
+          <Typography variant="body2" align="center">
+            No profile found, please login again
+          </Typography>
+          <div className={classes.buttons}>
+            <Button
+              variant="contained"
+              color="primary"
+              component={Link}
+              to="/login"
+            >
+              Login
+            </Button>
+            <Button
+              variant="contained"
+              color="secondary"
+              component={Link}
+              to="/signup"
+            >
+              Sign up
+            </Button>
+          </div>
+        </Paper>
+      )
     ) : (
-        <Paper className={classes.paper}>
-            <Typography variant="body2" align="center">
-                No profile found, please login again
-            </Typography>
-            <div className={classes.buttons}>
-                <Button variant="contained" color="primary" component={Link} to="/login">
-                    Login
-                </Button>
-                <Button variant="contained" color="secondary" component={Link} to="/signup">
-                    Sign up
-                </Button>
-            </div>
-        </Paper>
-    )) : (<p>loading...</p>)
+      <p>loading...</p>
+    );
 
     return profileMarkUp;
   }
@@ -90,9 +133,13 @@ const mapStateToProps = (state) => ({
   user: state.user,
 });
 
+const mapActionsToProps = { logoutUser, uploadImage };
+
 Profile.propTypes = {
+  logoutUser: PropTypes.func.isRequired,
+  uploadImage: PropTypes.func.isRequired,
   user: PropTypes.object.isRequired,
   classes: PropTypes.object.isRequired,
 };
 
-export default connect(mapStateToProps)(withStyles(styles)(Profile));
+export default connect(mapStateToProps, mapActionsToProps)(withStyles(styles)(Profile));
--- src/redux/actions/userActions.js
+++ src/redux/actions/userActions.js
@@ -65,6 +65,17 @@ export const getUserData = () => (dispatch) => {
     });
 };
 
+export const uploadImage = (formData) => (dispatch) => {
+  dispatch({ type: LOADING_USER })
+  axios.post('/user/image', formData)
+  .then(() => {
+    dispatch(getUserData());
+  })
+  .catch(err => {
+    console.log(err);
+  })
+}
+
 const setAuthorizationHeader = (token) => {
   const FBIdtoken = `Bearer ${token}`;
   localStorage.setItem("FBIdtoken", FBIdtoken);
